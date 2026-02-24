cc6e9e7c37dce6c3cd5f9a49e693edd27494ccad
skatzrskx55q <skatzr1997@gmail.com>
2026-02-24 16:44:00 +0700

    Update rag_ui.py

--- ui/rag_ui.py
+++ ui/rag_ui.py
@@ -1,4 +1,4 @@
-﻿import html
+import html
 
 import streamlit as st
 
@@ -16,8 +16,8 @@ def render(df):
                 for score, chunk in results:
                     st.markdown(
                         f"""
-                    <div style="border:1px solid #e0e0e0;border-radius:12px;padding:16px;margin-bottom:16px;background-color:#f9f9f9;box-shadow:0 2px 6px rgba(0,0,0,0.05);">
-                        <div style="font-size:13px;color:#888;">Релевантность: {score:.2f}</div>
+                    <div style="border:1px solid #e0e0e0;border-radius:12px;padding:16px;margin-bottom:16px;background-color:#f9f9f9;color:#333;box-shadow:0 2px 6px rgba(0,0,0,0.05);">
+                        <div style="font-size:13px;color:#666;">Релевантность: {score:.2f}</div>
                         <div style="margin-top:8px; white-space: pre-wrap;">{html.escape(chunk)}</div>
                     </div>
                     """,
